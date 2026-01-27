326ea99a1a2131602450d1d5ee0ad6729c01a0a7
John Corser <johnpc@umich.edu>
2026-01-27 12:18:46 -0500

    Initial Commit

---
+++ jpc.fit.native/jpc_fit_nativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  jpc_fit_nativeApp.swift
+//  jpc.fit.native
+//
+//  Created by John Corser on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct jpc_fit_nativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
